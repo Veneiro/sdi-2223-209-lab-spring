b2924b1d96639da57c08bb2c2b257a645d63c860
Veneiro <50624734+Veneiro@users.noreply.github.com>
2023-02-14 15:03:05 +0100

    SDI-2223-209-2.2-Trabajando con Controladores.

--- src/main/java/com/uniovi/sdi2223209spring/Sdi2223209SpringApplication.java
+++ src/main/java/com/uniovi/sdi2223209spring/Sdi2223209SpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Sdi2223209SpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Sdi2223209SpringApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/demo/Sdi2223209SpringApplicationTests.java
+++ src/test/java/com/example/demo/Sdi2223209SpringApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Sdi2223209SpringApplicationTests {
 
 	@Test
 	void contextLoads() {
