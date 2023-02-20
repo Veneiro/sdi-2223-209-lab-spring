cf1f7d7c8eeec42a42e6f28f906f06a0a8a09c6d
Veneiro <50624734+Veneiro@users.noreply.github.com>
2023-02-20 18:52:44 +0100

    fix

---
+++ src/main/java/com/uniovi/sdi2223209spring/repositories/TeacherRepository.java
@@ -0,0 +1,7 @@
+package com.uniovi.sdi2223209spring.repositories;
+
+import com.uniovi.sdi2223209spring.entities.Teacher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TeacherRepository extends CrudRepository<Teacher, String> {
+}
