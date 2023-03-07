bfd18f38558f5633bf27f3408a3559e831fd98eb
Veneiro <50624734+Veneiro@users.noreply.github.com>
2023-03-07 16:22:51 +0100

    SDI-2223-209-5.1-Clase principal.

--- src/test/java/com/uniovi/sdi2223209spring/Sdi2223209SpringApplicationTests.java
+++ src/test/java/com/uniovi/sdi2223209spring/Sdi2223209SpringApplicationTests.java
@@ -1,9 +1,14 @@
 package com.uniovi.sdi2223209spring;
 
+import com.uniovi.sdi2223209spring.pageobjects.PO_HomeView;
+import com.uniovi.sdi2223209spring.pageobjects.PO_Properties;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -13,8 +18,8 @@ class Sdi2223209SpringApplicationTests {
     //static String Geckodriver = "C:\\Path\\geckodriver-v0.30.0-win64.exe";
     static String Geckodriver = "C:\\Dev\\tools\\selenium\\geckodriver-v0.30.0-win64.exe";
     //static String PathFirefox = "/Applications/Firefox.app/Contents/MacOS/firefox-bin";
-	//static String Geckodriver = "/Users/USUARIO/selenium/geckodriver-v0.30.0-macos";
-	//Común a Windows y a MACOSX
+    //static String Geckodriver = "/Users/USUARIO/selenium/geckodriver-v0.30.0-macos";
+    //Común a Windows y a MACOSX
     static WebDriver driver = getDriver(PathFirefox, Geckodriver);
     static String URL = "http://localhost:8090";
 
@@ -25,80 +30,94 @@ class Sdi2223209SpringApplicationTests {
         return driver;
     }
 
-	@BeforeEach
-	public void setUp(){
-		driver.navigate().to(URL);
-	}
-	//Después de cada prueba se borran las cookies del navegador
-	@AfterEach
-	public void tearDown(){
-		driver.manage().deleteAllCookies();
-	}
-	//Antes de la primera prueba
-	@BeforeAll
-	static public void begin() {}
-	//Al finalizar la última prueba
-	@AfterAll
-	static public void end() {
-		//Cerramos el navegador al finalizar las pruebas
-		driver.quit();
-	}
+    @BeforeEach
+    public void setUp() {
+        driver.navigate().to(URL);
+    }
 
-	@Test
-	@Order(1)
-	void PR01(){
+    //Después de cada prueba se borran las cookies del navegador
+    @AfterEach
+    public void tearDown() {
+        driver.manage().deleteAllCookies();
+    }
 
-	}
-	@Test
-	@Order(2)
-	void PR02(){
+    //Antes de la primera prueba
+    @BeforeAll
+    static public void begin() {
+    }
 
-	}
-	@Test
-	@Order(3)
-	void PR03(){
+    //Al finalizar la última prueba
+    @AfterAll
+    static public void end() {
+        //Cerramos el navegador al finalizar las pruebas
+        driver.quit();
+    }
 
-	}
-	@Test
-	@Order(4)
-	void PR04(){
+    @Test
+    @Order(1)
+    void PR01A() {
+        PO_HomeView.checkWelcomeToPage(driver, PO_Properties.getSPANISH());
+    }
 
-	}
-	@Test
-	@Order(5)
-	void PR05(){
+    @Test
+    @Order(2)
+    void PR01B() {
+        List<WebElement> welcomeMessageElement = PO_HomeView.getWelcomeMessageText(driver,
+                PO_Properties.getSPANISH());
+        Assertions.assertEquals(welcomeMessageElement.get(0).getText(),
+                PO_HomeView.getP().getString("welcome.message", PO_Properties.getSPANISH()));
+    }
 
-	}
+    @Test
+    @Order(3)
+    void PR02() {
+        PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+    }
 
-	@Test
-	@Order(6)
-	void PR06(){
+    @Test
+    @Order(4)
+    void PR03() {
+        PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
 
-	}
+    }
 
-	@Test
-	@Order(7)
-	void PR07(){
+    @Test
+    @Order(5)
+    void PR04() {
+        PO_HomeView.checkChangeLanguage(driver, "btnSpanish", "btnEnglish",
+                PO_Properties.getSPANISH(), PO_Properties.getENGLISH());
 
-	}
+    }
 
-	@Test
-	@Order(8)
-	void PR08(){
+    @Test
+    @Order(6)
+    void PR05() {
 
-	}
+    }
 
-	@Test
-	@Order(9)
-	void PR09(){
+    @Test
+    @Order(7)
+    void PR06() {
 
-	}
+    }
 
-	@Test
-	@Order(10)
-	void PR10(){
+    @Test
+    @Order(8)
+    void PR07() {
 
-	}
+    }
+
+    @Test
+    @Order(9)
+    void PR08() {
+
+    }
+
+    @Test
+    @Order(10)
+    void PR09() {
+
+    }
 
     @Test
     void contextLoads() {
